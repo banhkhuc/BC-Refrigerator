6891a6fc32df1a30834b89e7e5de4b40089979f8
20020066 Phạm Quang Phong <20020066@vnu.edu.vn>
2022-12-27 02:30:54 +0700

    fix conflict

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import accountRouter from './account';
-
 import productlineRouter from './productLine';
-import accountRouter from './account';
 
 const router = Router();
 
 router.use('/account', accountRouter);
+router.use('/productline', productlineRouter);
 
 export default router;
